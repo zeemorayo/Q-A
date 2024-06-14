4c8cd98ef8381ff12b4984e8b981dfeb51dd2b31
Nifesi Ajikiola <153874112+zeemorayo@users.noreply.github.com>
2024-06-14 17:48:11 +0100

    Add files via upload

---
+++ right.js
@@ -0,0 +1,481 @@
+
+var quiz = {
+  "JS": [
+  {
+  "id": 1,
+  "question": "What killed Princess Diana?",
+  "options": [
+  {
+  "a": "A car crash",
+  "b": "Her driver",
+  "c": "Old age",
+  "d": "Cancer"
+  }
+  ],
+  "answer": "A car crash ",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 2,
+  "question": "What is the highest grossing film of all time",
+  "options": [
+  {
+  "a": "Avatar",
+  "b": "Black Panther",
+  "c": "Game of thrones",
+  "d": "Avengers: End game"
+  }
+  ],
+  "answer": "Avatar",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 3,
+  "question": "Who has the most grammys ever?",
+  "options": [
+  {
+  "a": "Adele",
+  "b": "Sia",
+  "c": "Beyonce",
+  "d": "2pac"
+  }
+  ],
+  "answer": "Beyonce",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 4,
+  "question": "Which artist won the most grammy's in one night",
+  "options": [
+  {
+  "a": "2pac",
+  "b": "Micheal Jackson",
+  "c":"Beyonce",
+  "d":"Adele"
+
+  }
+  ],
+  "answer": "Micheal Jackson",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 5,
+  "question": "Which streaming platform created Stranger Things",
+  "options": [
+  {
+  "a": "Netflix",
+  "b": "Paramount",
+  "c": "Peacock",
+  "d": "Disney",
+  }
+  ],
+  "answer": "Netflix",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 6,
+  "question": "Who sang Rolling deep",
+  "options": [
+  {
+  "a": "The Beatles",
+  "b": "Adele",
+  "c": "Declan McKenna",
+  "d": "Beyonce"
+  }
+  ],
+  "answer": "Adele",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 7,
+  "question": "What was the first animated feature film produced by Disney?",
+  "options": [
+  {
+  "a": "Mickey Mouse",
+  "b": "Snow white",
+  "c": "Cinderella",
+  "d": "The Lion King"
+  }
+  ],
+  "answer": "Snow White",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 8,
+  "question": "Which movie features the quote, May the Force be with you",
+  "options": [
+  {
+  "a": "Guardians of the galaxy",
+  "b": "Star Wars",
+  "c": "Merlin",
+  "d": "Black Panther",
+  }
+  ],
+  "answer": "Star Wars",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 9,
+  "question": "Adam Levine is in which musical band?",
+  "options": [
+  {
+  "a": "Maroon 5",
+  "b": "The beatles",
+  "c": "BTS",
+  "d": "None of the above."
+  }
+  ],
+  "answer": "Maroon 5",
+  "score": 0, 
+  "status": ""
+  },
+  {
+  "id": 10,
+  "question": "Who starred as Jack in the movie Titanic?",
+  "options": [
+  {
+  "a": "Adam Sandler ",
+  "b": "Leonardo DiCaprio",
+  "c": "Will Smith",
+  "d": "Jack Griffo",
+  }
+  ],
+  "answer": "Leonardo DiCaprio",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 11,
+  "question": "What is the name of Taylor Swift's first album",
+  "options": [
+  {
+  "a": "Taylor Swift",
+  "b": "Scarlet",
+  "c": "yes and?",
+  "d": "Queen"
+  }
+  ],
+  "answer": "Taylor Swift",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 12,
+  "question": "What TV show is known for the characters Ross, Rachel, Monica, Chandler, Joey, and Phoebe?",
+  "options": [
+  {
+  "a": "FRIENDS",
+  "b": "How i met your mother",
+  "c": "Bel-air",
+  "d": "Bob hearts Abishola"
+  }
+  ],
+  "answer": "FRIENDS",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 13,
+  "question": "What is the name of the kingdom where Elsa and Anna live in Frozen?",
+  "options": [
+  {
+  "a": "Buckingham",
+  "b": "Arandelle",
+  "c": "Fillmore",
+  "d": "Metroville"
+  }
+  ],
+  "answer": "Arandelle", 
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 14,
+  "question": " Which singer is known for the album 21?",
+  "options": [
+  {
+  "a": "21 Savage",
+  "b": "Adele",
+  "c": "Beyonce",
+  "d":"Sasha Sloane "
+  }
+  ],
+  "answer": "Adele",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 15,
+  "question": "Which famous pop star's fans are called Little Monster?",
+  "options": [
+  {
+  "a": "Olivia Rodrigo",
+  "b": "Lady Gaga",
+  "c": "Nicki Minaj",
+  "d": "Lil Uzi Vert"
+  }
+  ],
+  "answer": "Lady Gaga",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 16,
+  "question": "What is the name of the ice planet in Star Wars: The Empire Strikes Back?",
+  "options": [
+  {
+  "a": "Hoth",
+  "b": "Starville",
+  "c": "Arandelle",
+  "d": "Wakanda"
+  }
+  ],
+  "answer": "Hoth",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 17,
+  "question": "What is the name of Batman's butler?",
+  "options": [
+  {
+  "a": " Alfred Pennyworth.",
+  "b": "Greg",
+  "c": "Topper",
+  "d": "Craige"
+  }
+  ],
+  "answer": " Alfred Pennyworth.",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 18,
+  "question": "Who played Jack Sparrow in the Pirates of the Caribbean series?",
+  "options": [
+  {
+  "a": "Tom Holland",
+  "b": "Johnny Depp",
+  "c": "Theo James",
+  "d": "Chris Evans"
+  }
+  ],
+  "answer": "Johnny Depp",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 19,
+  "question": " What is the name of the dragon in Disney's Mulan?",
+  "options": [
+  {
+  "a": "Mushu",
+  "b": "Miranda",
+  "c":"Mulan",
+  "d":"Greg"
+  }
+  ],
+  "answer": "Mushu",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 20,
+  "question": " In Game of Thrones, what is the name of Arya Stark's sword?",
+  "options": [
+  {
+  "a": "Needle",
+  "b": "Magna",
+  "c": "Clinton",
+  "d": "Little Dragon"
+  }
+  ],
+  "answer": "Needle",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 21,
+  "question": "Who played the role of the Genie in Aladdin?",
+  "options": [
+  {
+  "a": "Will Smith",
+  "b": "Chris Evans",
+  "c": "Zendaya",
+  "d": "Dwayne THE ROCK Johnson"
+  }
+  ],
+  "answer": "Will Smith",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 22,
+  "question": "What is the name of Elon Musk's electric car company?",
+  "options": [
+  {
+  "a": "X",
+  "b": "Meta",
+  "c": "Tesla",
+  "d": "Musk"
+  }
+  ],
+  "answer": "Tesla",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 23,
+  "question": "Who was the voice of the disney animated character Maui",
+  "options": [
+  {
+  "a": "Dwayne Johnson",
+  "b": "Kevin Hart",
+  "c": "Johnny Depp",
+  "d": "None of the above"
+  }
+  ],
+  "answer": "Dwayne Johnson",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 24,
+  "question": "Which Nigerian artist won the Grammy Award for Best Global Music Album in 2021?",
+  "options": [
+  {
+  "a": "Davido",
+  "b": "Burna Boy",
+  "c": "Yemi Alade",
+  "d": "Wizkid"
+  }
+  ],
+  "answer": "Burna Boy",
+  "score": 0,
+  "status": ""
+  },
+  {
+  "id": 25,
+  "question": " What reality TV show did Cardi B first gain fame from?",
+  "options": [
+  {
+  "a": "Love and HipHop",
+  "b": "Keeping up with the Kardashians",
+  "c": "The strip club",
+  "d": "Cardi and friends"
+  }
+  ],
+  "answer": "Love an HipHop",
+  "score": 0,
+  "status": ""
+  }
+  ]
+  }
+  var quizApp = function () {
+  this.score = 0;
+  this.qno = 1;
+  this.currentque = 0;
+  var totalque = quiz.JS.length;
+  this.displayQuiz = function (cque) {
+  this.currentque = cque;
+  if (this.currentque < totalque) {
+  $("#tque").html(totalque);
+  $("#previous").attr("disabled", false);
+  $("#next").attr("disabled", false);
+  $("#qid").html(quiz.JS[this.currentque].id + '.');
+  $("#question").html(quiz.JS[this.currentque].question);
+  $("#question-options").html("");
+  for (var key in quiz.JS[this.currentque].options[0]) {
+  if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+  $("#question-options").append(
+  "<div class='form-check option-block'>" +
+  "<label class='form-check-label'>" +
+  "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+  quiz.JS[this.currentque].options[0][key] +
+  "</span></label>"
+  );
+  }
+  }
+  }
+  if (this.currentque <= 0) {
+  $("#previous").attr("disabled", true);
+  }
+  if (this.currentque >= totalque) {
+  $('#next').attr('disabled', true);
+  for (var i = 0; i < totalque; i++) {
+  this.score = this.score + quiz.JS[i].score;
+  }
+  return this.showResult(this.score);
+  }
+  }
+  this.showResult = function (scr) {
+  $("#result").addClass('result');
+  $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+  for (var j = 0; j < totalque; j++) {
+  var res;
+  if (quiz.JS[j].score == 0) {
+  res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+  } else {
+  res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+  }
+  $("#result").append(
+  '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+  '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+  '<div class="last-row"><b>Score:</b> &nbsp;' + res +
+  '</div>'
+  );
+  }
+  }
+  this.checkAnswer = function (option) {
+  var answer = quiz.JS[this.currentque].answer;
+  option = option.replace(/</g, "&lt;") //for <
+  option = option.replace(/>/g, "&gt;") //for >
+  option = option.replace(/"/g, "&quot;")
+  if (option == quiz.JS[this.currentque].answer) {
+  if (quiz.JS[this.currentque].score == "") {
+  quiz.JS[this.currentque].score = 1;
+  quiz.JS[this.currentque].status = "correct";
+  }
+  } else {
+  quiz.JS[this.currentque].status = "wrong";
+  }
+  }
+  this.changeQuestion = function (cque) {
+  this.currentque = this.currentque + cque;
+  this.displayQuiz(this.currentque);
+  }
+  }
+  var jsq = new quizApp();
+  var selectedopt;
+  $(document).ready(function () {
+  jsq.displayQuiz(0);
+  $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+  //var radio = $(this).find('input:radio');
+  $(this).prop("checked", true);
+  selectedopt = $(this).val();
+  });
+  });
+  $('#next').click(function (e) {
+  e.preventDefault();
+  if (selectedopt) {
+  jsq.checkAnswer(selectedopt);
+  }
+  jsq.changeQuestion(1);
+  });
+  $('#previous').click(function (e) {
+  e.preventDefault();
+  if (selectedopt) {
+  jsq.checkAnswer(selectedopt);
+  }
+  jsq.changeQuestion(-1);
+  });
+  resultDiv.innerHTML += '<button id="restart">Restart Quiz</button>';
+  document.getElementById('restart').addEventListener('click', this.restartQuiz.bind(this));
